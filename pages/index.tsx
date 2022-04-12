cad7d5f71caf0b9866eb8c3f4d4b9460144ebf92
Christer Gundersen <46853403+christer-io@users.noreply.github.com>
2022-04-12 11:10:23 +0200

    Fixed alt text and lang atribute

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,7 +41,7 @@ export default function Home ({ posts }: Props) {
         {posts.map((post) => (
           <Link key={post._id} href={`/post/${post.slug.current}`}>
             <div className="border rounded-lg group cursor-pointer overflow-hidden">
-              <img className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out" src={urlFor(post.mainImage).url()!} alt="" />
+              <img className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out" src={urlFor(post.mainImage).url()!} alt={post.title} />
               <div className="flex justify-between p-5 bg-white">
                 <div>
                   <p className="text-lg font-bold">{post.title}</p>
