e99ed9251579071ed75f293c211d55b392b8ab42
Christer Gundersen <46853403+christer-io@users.noreply.github.com>
2022-04-12 11:18:25 +0200

    Fixed heading for accessibilty

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,15 +9,15 @@ function Header() {
             <h1 className="text-3xl cursor-pointer">ObiOne.io</h1>
             </Link>
             <div className="hidden md:inline-flex items-center space-x-5">
-                <h3>About</h3>
-                <h3>Contact</h3>
-                <h3 className="text-white bg-green-800 px-4 py-1 rounded-full">Projects</h3>
+                <h2>About</h2>
+                <h2>Contact</h2>
+                <h2 className="text-white bg-green-800 px-4 py-1 rounded-full">Projects</h2>
             </div>
         </div>
         
         <div className="flex items-center space-x-5 text-green-800" >
-        <h3 className="border px-4 py-1 rounded-full cursor-pointer border-green-600">Open Education</h3>
-            <h3 className="border px-4 py-1 rounded-full cursor-pointer border-green-600">Open Source</h3>
+        <h2 className="border px-4 py-1 rounded-full cursor-pointer border-green-600">Open Education</h2>
+            <h2 className="border px-4 py-1 rounded-full cursor-pointer border-green-600">Open Source</h2>
         </div>
     
         
