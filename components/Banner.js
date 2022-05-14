53e4a3908e3ea8b9efd65522c960ae53a0e02ace
Christer Gundersen <46853403+christer-io@users.noreply.github.com>
2022-05-14 10:41:58 +0000

    Color fix

--- components/Banner.js
+++ components/Banner.js
@@ -3,12 +3,12 @@ import Link from "next/link"
 
 function Banner() {
   return (
-    <div className="flex justify-between items-center text-darkdarkblue py-10 md: lg:py-0 rounded-lg">
+    <div className="flex justify-between items-center text-blue py-10 md: lg:py-0 rounded-lg">
     <div className="px-10 space-y-5">
       <h1 className="text-4xl sm:text-6xl max-w-xl font-sans">
-        <span className="decoration-4 text-blue">Open source is a methodology 
+        <span className="decoration-4 text-darkdarkblue">Open source is a methodology 
         </span>{" "} and a social movement!</h1>
-        <h2 className="text-blue">
+        <h2 className="text-darkdarkblue">
          By embracing an open mindset, philosofy and metodology we can solve the global grand challanges in more transparent and democratic ways.
         </h2>
         
