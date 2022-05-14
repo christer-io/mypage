f9c1030546175ff622f8012b75d64cca6c6aef4e
Christer Gundersen <46853403+christer-io@users.noreply.github.com>
2022-05-14 16:39:29 +0000

    Update meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,9 @@ export default function Home ({ posts }: Props) {
       <Head>
         <title>ObiOne</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="obione" content="Information about open source, open content"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta name="keywords" content="open source, open data, open content"></meta>
+        <meta name="description" content="Open source, open data, open education and digital public goods"></meta>
       </Head>
       <Header />
       <Banner />
