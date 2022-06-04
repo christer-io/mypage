508bc6c660267af9774a07be2e2aa45bd12f3254
Christer Gundersen <46853403+christer-io@users.noreply.github.com>
2022-06-04 22:39:51 +0200

    Universal design issues fixed

--- components/Banner.js
+++ components/Banner.js
@@ -8,9 +8,9 @@ function Banner() {
       <h1 className="text-5xl sm:text-7xl xl:text-8xl font-sans">
         <span className="decoration-4 text-darkdarkblue ">Open source is a methodology 
         </span>{" "} and a social movement!</h1>
-        <h3 className="text-darkdarkblue">
+        <h2 className="text-darkdarkblue">
          With an philosofy and metodology we can solve the global grand challanges in more effective, transparent and democratic ways.
-        </h3>
+        </h2>
         <div className="space-x-3 pt-5 pb-5">
           <button class="bg-blue-500 bg-buttonblue text-white font-bold py-2 px-4 rounded pl-10 pr-10">
             Open source
