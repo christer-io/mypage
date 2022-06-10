fad3e4a9d3f005d0a61cb2a76d4c51d55263566c
Christer Gundersen <46853403+christer-io@users.noreply.github.com>
2022-06-10 21:58:23 +0200

    Banner back in align-center

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,7 +32,7 @@ export default function Home ({ posts }: Props) {
       <Header />
       <Banner part1="Open source is a methodology" part2="and a social movement!" subtext="With an open philosofy and metodology we can solve the global grand challanges in more effective, transparent and democratic ways."/>
 
-      <div className='text-3xl text-left font-sans text-darkdarkblue pl-10'> 
+      <div className='text-3xl text-center font-sans text-darkdarkblue pl-10'> 
       <h2>The elements of open </h2> 
       <h3 className="text-base">The open movement is built on a set of core elements.</h3></div>
       {/* posts */}
