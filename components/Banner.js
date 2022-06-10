4f17a5819a20cfdf0c7bc748ce42182685426286
Christer Gundersen <46853403+christer-io@users.noreply.github.com>
2022-06-10 22:12:34 +0200

    Update Banner.js

--- components/Banner.js
+++ components/Banner.js
@@ -6,7 +6,7 @@ function Banner({ part1, part2, subtext }) {
   return (
   <div className=" text-blue text-center pt-10 pb-10">
     <div className="px-5 space-y-5 ">
-      <h1 className="pl-10 pr-10 text-3xl sm:text-7xl xl:text-8xl font-sans">
+      <h1 className="pl-10 pr-10 text-4xl sm:text-7xl xl:text-8xl font-sans">
         <span className="decoration-4 text-darkdarkblue "> {part1} 
         </span>{" "} {part2}</h1>
         <h2 className="text-darkdarkblue text-center ">
